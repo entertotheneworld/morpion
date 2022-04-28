fbb7eb04d5aaeba28ee03bd0e109af146d6c407a
entertotheneworld <tristan.greffe@hotmail.com>
2022-04-28 22:36:46 +0200

    Best show board

--- lib/views/show.rb
+++ lib/views/show.rb
@@ -3,21 +3,21 @@ class Show
   # AFFICHAGE DU JEU
   def show_board(board)
 
-    puts "---|----------------------------".blue.on_green
-    puts "   |         |         |        ".blue.on_green
-    puts " A |    #{board.board_cases[0].board_case_string}    |    #{board.board_cases[3].board_case_string}    |    #{board.board_cases[6].board_case_string}   ".blue.on_green 
-    puts "   |         |         |        ".blue.on_green
-    puts "---|----------------------------".blue.on_green
-    puts "   |         |         |        ".blue.on_green
-    puts " B |    #{board.board_cases[1].board_case_string}    |    #{board.board_cases[4].board_case_string}    |    #{board.board_cases[7].board_case_string}   ".blue.on_green 
-    puts "   |         |         |        ".blue.on_green
-    puts "---|----------------------------".blue.on_green
-    puts "   |         |         |        ".blue.on_green
-    puts " C |    #{board.board_cases[2].board_case_string}    |    #{board.board_cases[5].board_case_string}    |    #{board.board_cases[8].board_case_string}   ".blue.on_green 
-    puts "   |         |         |        ".blue.on_green
-    puts "---|----------------------------".blue.on_green
-    puts "   |    1    |    2    |    3   ".blue.on_green 
-    puts "   |         |         |        ".blue.on_green
+    puts "---|----------------------------".white.on_green
+    puts "   |         |         |        ".white.on_green
+    puts " A |    #{board.board_cases[0].board_case_string}    |    #{board.board_cases[3].board_case_string}    |    #{board.board_cases[6].board_case_string}   ".white.on_green 
+    puts "   |         |         |        ".white.on_green
+    puts "---|----------------------------".white.on_green
+    puts "   |         |         |        ".white.on_green
+    puts " B |    #{board.board_cases[1].board_case_string}    |    #{board.board_cases[4].board_case_string}    |    #{board.board_cases[7].board_case_string}   ".white.on_green 
+    puts "   |         |         |        ".white.on_green
+    puts "---|----------------------------".white.on_green
+    puts "   |         |         |        ".white.on_green
+    puts " C |    #{board.board_cases[2].board_case_string}    |    #{board.board_cases[5].board_case_string}    |    #{board.board_cases[8].board_case_string}   ".white.on_green 
+    puts "   |         |         |        ".white.on_green
+    puts "---|----------------------------".white.on_green
+    puts "   |    1    |    2    |    3   ".white.on_green 
+    puts "   |         |         |        ".white.on_green
   end
 
 end
